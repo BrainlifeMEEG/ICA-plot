e1b73e7b58527cb707e3531cf943a4dc7fe06cfd
Maximilien Chaumon <maximilien.chaumon@protonmail.com>
2026-03-17 13:34:11 +0100

    Initialize product_items earlier and update add_info_to_product calls

--- main.py
+++ main.py
@@ -48,7 +48,10 @@ config = load_config()
 fname = config['ica']
 ica = mne.preprocessing.read_ica(fname, verbose=None)
 print(f'Loaded ICA with {ica.n_components} components')
-add_info_to_product(f'ICA object loaded with {ica.n_components} components')
+
+# Initialize product items
+product_items = []
+add_info_to_product(product_items, f'ICA object loaded with {ica.n_components_} components')
 
 # == PLOT COMPONENTS TOPOGRAPHY ==
 plt.figure(figsize=(16, 10))
@@ -72,6 +75,6 @@ report.save(os.path.join('out_report', 'report_ica.html'), overwrite=True)
 print('Report saved to out_report/report_ica.html')
 
 # == CREATE PRODUCT.JSON ==
-create_product_json()
-add_image_to_product(components_fig_path, 'ICA Components')
-add_info_to_product(f'Visualized {ica.n_components} ICA components', 'success')
+add_image_to_product(product_items, 'ICA Components', filepath=components_fig_path)
+add_info_to_product(product_items, f'Visualized {ica.n_components_} ICA components', 'success')
+create_product_json(product_items)
